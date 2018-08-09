646a06faa12948fba3c634667eb8c4da0f1dd788
PriNT2357 <marvinrulesmars@gmail.com>
2018-08-08 23:25:48 -0400

    Change up downloads
    Looks like curl won't work since files are too big.
    Direct linking with a generic file name will have to suffice.

--- content_movie.php
+++ content_movie.php
@@ -1,6 +1,6 @@
 <?php 
 	$data = $section->getAllMovies();
-	//echo "<pre>".print_r($data[0])."</pre>";
+	echo "<pre>".print_r($data[0])."</pre>";
 ?>
 <div class="row">
 	  <div class="col-md-1">#</div>
@@ -11,17 +11,21 @@
 	  <div class="col-md"  >Summary</div>
 </div>
 <?php foreach ($data as $i => $item) { ?>
+	<?php $filename = array_pop(explode("/", $item->getMedia()->getFiles()[0]->getFile()));?>
+	<?php $linktofile = $server->getAddress() . ":" . $server->getPort() . $item->getMedia()->getFiles()[0]->getKey();?>
+
 	<div class="row">
 		<div class="col-md-1"><?php echo $i; ?></div>
 		<div class="col-md-3"><?php echo htmlspecialchars($item->getTitle() . " (" . $item->getYear() . ")"); ?></div>
 		<div class="col-md-1"><?php echo htmlspecialchars($item->getMedia()->getVideoResolution()); ?></div>
 		<div class="col-md-1"><?php echo htmlspecialchars(round($item->getMedia()->getFiles()[0]->getSize()/1024/1024/1024, 2)); ?></div>
 		<div class="col-md-3">
-			<a target="_blank" href="//<?php echo htmlspecialchars($server->getAddress() . ":" . $server->getPort() . $item->getMedia()->getFiles()[0]->getKey()); ?>">Direct</a> - 
-			<a target="_blank" href="download.php?sectionkey=<?php echo urlencode($section->getTitle()); ?>&key=<?php echo urlencode($item->getKey()); ?>">Via DL Page</a>
+			<a download="MyFile.mkv" href="blob://<?php echo htmlspecialchars($linktofile); ?>" data-downloadurl="application/octet-stream:MyFile.mkv:blob://<?php echo htmlspecialchars($linktofile); ?>" >ByFileName</a>
+			<a download="<?php echo htmlspecialchars($filename);?>" href="//<?php echo htmlspecialchars($linktofile); ?>">Direct</a> - 
+			<a href="download.php?sectionkey=<?php echo urlencode($section->getTitle()); ?>&key=<?php echo urlencode($item->getKey()); ?>">Via DL Page</a>
 		</div>
 		<div class="col-md"  ><?php echo htmlspecialchars(substr($item->getSummary(), 0, 50)). "..."; ?></div>
 	</div>
 <?php 
-if ($i > 10) break;
+if ($i > 50) break;
 } ?>
--- download.php
+++ download.php
@@ -16,11 +16,24 @@ if ($sectionType === "movie") {
 	$filename = array_pop(explode("/", $movie->getMedia()->getFiles()[0]->getFile()));
 	$size = $movie->getMedia()->getFiles()[0]->getSize();
 }
-$url = "//" . $server->getAddress() . ":" . $server->getPort();
+$url = "http://" . $server->getAddress() . ":" . $server->getPort();
 $url .= $movie->getMedia()->getFiles()[0]->getKey();
 
-header("x-plex-token: oBKsRxbkfxatfHyyzoXq");
-header("location: " . $url);
+
+//~ $ch = curl_init($url);
+//~ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	//~ 'x-plex-token:' . $server->getToken()
+	//~ ));
+//~ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//~ $response = curl_exec($ch);
+//~ if ($response === FALSE) {
+	//~ throw new Plex_Exception_Machine(
+		//~ 'CURL_ERROR',
+		//~ array(curl_errno($ch), curl_error($ch))
+	//~ );
+//~ }
+
 header('Content-Description: File Transfer');
 header('Content-Type: application/octet-stream');
 header("Content-Disposition: attachment; filename='" . $filename . "'"); 
@@ -30,6 +43,7 @@ header('Connection: Keep-Alive');
 header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 //~ header('Pragma: public');
 header('Content-Length: ' . $size);
+//~ echo $response;
 //~ echo "<pre>".print_r($movie, true)."</pre>";
 //~ echo $url;
 //~ echo "<br>";
